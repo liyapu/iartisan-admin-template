132f6926c5ea5d984a422ba9e9cbc96ae3ff9b9b
King <wangkechaodream@163.com>
2018-03-23 11:01:43 +0800

    upgrade

--- src/main/java/org/iartisan/admin/template/authentication/AuthenticationServiceImpl.java
+++ src/main/java/org/iartisan/admin/template/authentication/AuthenticationServiceImpl.java
@@ -1,6 +1,8 @@
 package org.iartisan.admin.template.authentication;
 
 import org.iartisan.admin.template.authentication.support.dbm.model.SystemUserDO;
+import org.iartisan.admin.template.authentication.support.service.MenuSupportService;
+import org.iartisan.admin.template.authentication.support.service.ResourceSupportService;
 import org.iartisan.admin.template.authentication.support.service.UserSupportService;
 import org.iartisan.runtime.web.authentication.AuthenticationService;
 import org.iartisan.runtime.web.authentication.MenuTree;
@@ -23,6 +25,12 @@ public class AuthenticationServiceImpl extends AuthenticationService {
     @Autowired
     private UserSupportService userService;
 
+    @Autowired
+    private ResourceSupportService resourceSupportService;
+
+    @Autowired
+    private MenuSupportService menuSupportService;
+
     @Override
     protected RealmBean getRealmBean(String userName, String userPwd) {
         //判断用户名密码是否正确
@@ -35,13 +43,14 @@ public class AuthenticationServiceImpl extends AuthenticationService {
         bean.setUserName(userDO.getUserName());
         bean.setUserId(userDO.getUserId());
         //加载用户菜单列表
-        List<MenuTree> trees = userService.getMenus(userDO.getUserId());
+        List<MenuTree> trees = menuSupportService.getMenus(userDO.getUserId());
         bean.setMenuTrees(trees);
         return bean;
     }
 
     @Override
     protected Set<String> getPermissions(String userId) {
-        return null;
+        //通过userId 获取可操作权限列表
+        return resourceSupportService.getPermissionsByUserId(userId);
     }
 }
