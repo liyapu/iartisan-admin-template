58d9e4e1d7e29f184b91b4d25f3932d597194bd4
King <wangkechaodream@163.com>
2018-07-12 16:20:18 +0800

    pre 流程图展示

--- src/main/resources/static/assets/iartisan/js/activiti/workflow/start/workflowStart_index.js
+++ src/main/resources/static/assets/iartisan/js/activiti/workflow/start/workflowStart_index.js
@@ -5,6 +5,7 @@ layui.config({
     var urls = {
         addDataPage: "/activiti/workflow/start/addDataPage",
         queryPageData: "/activiti/workflow/start/owner/queryPageData",
+        getProcessTrace: "/activiti/workflow/start/owner/queryPageData"
     };
 
     $("#btnStart").on('click', function () {
@@ -55,7 +56,7 @@ layui.config({
                 {
                     title: '操作',
                     templet: function () {
-                        var html = "<a class='layui-btn layui-btn-xs' lay-event='del'>查看流程图</a>";
+                        var html = "<a class='layui-btn layui-btn-xs' lay-event='processImg'>查看流程图</a>";
                         html += "<a class='layui-btn layui-btn-danger layui-btn-xs' lay-event='del'>撤回</a>";
                         return html;
                     }
@@ -64,63 +65,50 @@ layui.config({
         });
     }
 
-    function loadProcessDefineImg() {
-        var processInstanceId = $("#processInstanceId").val();
-        var imageUrl = page.ajaxURL.getProcessDefineImg + "?processInstanceId=" + processInstanceId;
-        $('#workflowTraceDiv img').attr('src', imageUrl);
-        //渲染流程节点
-        $.getJSON(page.ajaxURL.getProcessTrace + "?processInstanceId=" + processInstanceId, function (data) {
-            var infos = data.dataList;
-            var positionHtml = "";
-            // 生成图片
-            $.each(infos, function (i, v) {
-                var vars = v.vars;
-                var $positionDiv = $('<div/>', {
-                    'class': 'activity-attr'
-                }).css({
-                    position: 'absolute',
-                    left: (v.x ),
-                    top: (v.y ),
-                    width: (v.width ),
-                    height: (v.height ),
-                    backgroundColor: 'black',
-                    opacity: 0,
-                });
-                // 节点边框
-                var $border = $('<div/>', {
-                    'class': 'activity-attr-border'
-                }).css({
-                    position: 'absolute',
-                    left: (v.x ),
-                    top: (v.y ),
-                    width: (v.width ),
-                    height: (v.height ),
-                }).attr("var", JSON.stringify(vars));
-                if (v.currentActiviti) {
-                    $border.addClass('ui-corner-all-12').css({
-                        border: '3px solid red'
+    table.on('tool(_table)', function (obj) {
+        var layEvent = obj.event, data = obj.data;
+        if (layEvent == 'processImg') {
+            var processInstanceId = $("#processInstanceId").val();
+            var imageUrl = page.ajaxURL.getProcessDefineImg + "?processInstanceId=" + processInstanceId;
+            $('#workflowTraceDiv img').attr('src', imageUrl);
+            //渲染流程节点
+            $.getJSON(page.ajaxURL.getProcessTrace + "?processInstanceId=" + processInstanceId, function (data) {
+                var infos = data.dataList;
+                var positionHtml = "";
+                // 生成图片
+                $.each(infos, function (i, v) {
+                    var vars = v.vars;
+                    var $positionDiv = $('<div/>', {
+                        'class': 'activity-attr'
+                    }).css({
+                        position: 'absolute',
+                        left: (v.x ),
+                        top: (v.y ),
+                        width: (v.width ),
+                        height: (v.height ),
+                        backgroundColor: 'black',
+                        opacity: 0,
                     });
-                }
-                positionHtml += $positionDiv.outerHTML() + $border.outerHTML();
+                    // 节点边框
+                    var $border = $('<div/>', {
+                        'class': 'activity-attr-border'
+                    }).css({
+                        position: 'absolute',
+                        left: (v.x ),
+                        top: (v.y ),
+                        width: (v.width ),
+                        height: (v.height ),
+                    }).attr("var", JSON.stringify(vars));
+                    if (v.currentActiviti) {
+                        $border.addClass('ui-corner-all-12').css({
+                            border: '3px solid red'
+                        });
+                    }
+                    positionHtml += $positionDiv.outerHTML() + $border.outerHTML();
 
+                });
+                $('#processImageBorder').html(positionHtml);
             });
-            // alert(positionHtml);
-            $('#processImageBorder').html(positionHtml);
-            // 设置每个节点的data
-            /*$('#workflowTraceDiv .activity-attr').each(function(i, v) {
-             $(this).data('vars', varsArray[i]);
-             });*/
-            /*$(".activity-attr-border").mouseover(function () {
-                var top = $(this).offset().top + $(this).height() + 10;
-                var data = $(this).attr("var");
-                var line1 = '<div class="row clearfix"><label class="movelabel">' + data + '</div>';
-                var htmlcontent = "<div class='moveonStyle' style='position: absolute;top:" + top + "px; left:" + $(this).offset().left + "px;'>" + line1 + "</div>";
-                $('body').append(htmlcontent);
-            })
-            $(".activity-attr-border").mouseleave(function () {
-                $(".moveonStyle").remove()
-            })*/
-        });
-    }
-
+        }
+    });
 });
