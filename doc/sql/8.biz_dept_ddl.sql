4a5ef51494b866ac394493473578a54d432ebc17
King <wangkechaodream@163.com>
2019-09-16 10:31:49 +0800

    sql脚本

---
+++ doc/sql/8.biz_dept_ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `biz_dept` (
+  `DEPT_ID` varchar(48) NOT NULL COMMENT '主键',
+  `DEPT_NAME` varchar(50) NOT NULL COMMENT '部门名',
+  `DEPT_PATH` varchar(50) DEFAULT NULL COMMENT '部门树',
+  `DEPT_PARENT` varchar(50) DEFAULT NULL COMMENT '上一级部门',
+  `STATUS` varchar(1) NOT NULL DEFAULT 'E' COMMENT '数据状态',
+  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `UPDATE_TIME` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`DEPT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='部门表'
